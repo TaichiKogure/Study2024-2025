a6fe9541109b124a9cb52b8615f08e582364b79e
koguretaichi <taichi.kogure1105@gmail.com>
2024-02-08 21:23:36 +0900

    Implement MNIST data loading and neural network training
    Integrated TensorFlow's MNIST dataset into TF01_study.py for testing purposes. The update also includes the creation and training of a sequential model, with detailed steps for data normalization, model compilation, model training, and its evaluation. Optimized the model using the 'adam' optimizer and 'sparse_categorical_crossentropy' loss function.

--- tensorflowStudy/TF01_study.py
+++ tensorflowStudy/TF01_study.py
@@ -244,3 +244,36 @@ def train_neural_network(x):
 train_neural_network(x)
 
 
+#%%
+
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data() #データの読み込み
+x_train, x_test = x_train / 255.0, x_test / 255.0 #データの正規化
+
+
+# モデルの構築
+# Sequential API
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
+
+# optimizer, loss, metricsの設定（学習設定）
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# 学習
+model.fit(x_train, y_train, epochs=5)
+
+# 評価
+test_loss, test_acc = model.evaluate(x_test,  y_test, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+
