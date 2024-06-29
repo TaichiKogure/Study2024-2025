1c142bb0776009c82f1c7bff9d12cfb13ac11dd6
koguretaichi <taichi.kogure1105@gmail.com>
2024-06-29 10:48:30 +0800

    Add animation, randomness, and plotting to LED script
    In this commit, extra functionality has been added to the 'RaspPi Lesson1.py' script. Randomness has been introduced in the LED colors and blink duration, along with an animation of the LED's brightness over time. A plotting function has been introduced to visualize this data.

--- RaspPi Lesson1.py
+++ RaspPi Lesson1.py
@@ -1,5 +1,9 @@
 from gpiozero import PWMLED
 from time import sleep
+import random
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
 
 colors = [0xFF00, 0x00FF, 0x0FF0, 0xF00F]
 makerobo_pins = (17, 18)
@@ -13,24 +17,63 @@ def makerobo_pwn_map(x, in_min, in_max, out_min, out_max):
 def makerobo_set_Color(col):
     R_val = col >> 8
     G_val = col & 0x00FF
-    R_val = makerobo_pwn_map(R_val, 0,255,0,100)
-    G_val = makerobo_pwn_map(G_val, 0,255,0,100)
+    R_val = makerobo_pwn_map(R_val, 0, 255, 0, 100)
+    G_val = makerobo_pwn_map(G_val, 0, 255, 0, 100)
 
-    p_R.value = (R_val)/100.0
-    p_G.value = (G_val)/100.0
+    # 新たに、R_valとG_valをランダムに調整します
+    R_val *= random.uniform(0, 1)
+    G_val *= random.uniform(0, 1)
+
+    p_R.value = (R_val) / 100.0
+    p_G.value = (G_val) / 100.0
+
 
 def makerobo_roop():
+    start_time = time.time()
     while True:
         for col in colors:
-            makerobo_set_Color(col)
-            sleep(0.05)
+            brightness = random.uniform(0, 1)
+            makerobo_set_Color(col, brightness)
+            blink_time = random.uniform(0.1, 1)
+            sleep(blink_time)
 
+            elapsed_time = time.time() - start_time
+            record_list.append(TCB_Record(elapsed_time, col, brightness))
+
+            makerobo_set_Color(0, 0)
+            off_time = random.uniform(0.1, 1)
+            sleep(off_time)
+
+            elapsed_time = time.time() - start_time
+            record_list.append(TCB_Record(elapsed_time, 0, 0))
+
+
+
+def make_plot():
+    # Initialize figure
+    fig, ax = plt.subplots()
+
+    # Update function for the animation
+    def update(num):
+        times = [record.time for record in record_list]
+        brightnesses = [record.brightness for record in record_list]
+        ax.clear()
+        ax.plot(times, brightnesses)
+        ax.set_xlabel('Time (s)')
+        ax.set_ylabel('Brightness')
+        ax.set_title('Brightness over Time')
+
+    ani = animation.FuncAnimation(fig, update, frames=100, repeat=True)
+    plt.show()
+
+
+if __name__ == "__main__":
+    try:
+        make_plot()  # call plot function
+        makerobo_roop()
+    except KeyboardInterrupt:
+        makerobo_destroy()
 def makerobo_destroy():
     p_G.close()
     p_R.close()
 
-if __name__ == "__main__":
-    try:
-        makerobo_roop()
-    except KeyboardInterrupt:
-        makerobo_destroy()
