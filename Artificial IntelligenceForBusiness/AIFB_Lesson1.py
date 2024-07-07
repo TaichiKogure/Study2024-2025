b1af27bf989e830208a5baaa6b34c1ec0f77b97c
koguretaichi <taichi.kogure1105@gmail.com>
2024-07-07 11:19:56 +0800

    Add initial code for AIFB Lesson 1
    This commit introduces the basic structure for the AI for Business Lesson 1. It sets up the Q-learning parameters, defines the environment, and outlines the plan for building the AI solution and its production phase.

---
+++ Artificial IntelligenceForBusiness/AIFB_Lesson1.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+# Setting the parameters gamma and alpha for the Q-learning
+gamma = 0.75
+alpha = 0.9
+
+# Part 1 Defining the environment
+# Defining the status
+
+location_to_state = {'A': 0,
+                     'B': 1,
+                     'C': 2,
+                     'D': 3,
+                     'E': 4,
+                     'F': 5,
+                     'G': 6,
+                     'H': 7,
+                     'I': 8,
+                     'J': 9,
+                     'K': 10,
+                     'L': 11}
+# Dfining the actions
+actions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+#Defining the rewards
+
+
+# Part 2 - Building the AI solution with Q-learning
+
+
+# Part 3 - Going into Production
