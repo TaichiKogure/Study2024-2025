9868e0103489884a33894bb66b7908057f67d76f
koguretaichi <taichi.kogure1105@gmail.com>
2024-07-22 21:16:16 +0800

    Enhance Environment update method in AIFB Lesson 2
    The Environment update method is now improved with additional computations relevant to the AI operation in the script 'AIFB_Lesson2_MinimizingCosts-1.py'. This includes computing the energy spent by the server's cooling system when there is no AI, scaling the reward, and updating the state variables such as atmospheric temperature, number of users, and rate of data. These changes will help in determining the effectiveness of AI in optimizing energy consumption.

--- Artificial IntelligenceForBusiness/AIFB_Lesson2_MinimizingCosts-1.py
+++ Artificial IntelligenceForBusiness/AIFB_Lesson2_MinimizingCosts-1.py
@@ -36,8 +36,45 @@ class Environment(Object):
 
     # MAKING A METHOD THAT UPDATES THE ENVIRONMENT RIGHT AFTER THE AI PLAYS AN ACTION
     def update_env(self, direction, energy_ai, month):
+
         # GETTING THE REWARD
         # Computing the energy spent by the server's cooling system when there is no AI
+        energy_noai = 0
+        if (self.temperature_noai < self.optimal_temperature[0]):
+            energy_noai = self.optimal_temperature[0] - self.temperature_noai
+            self.temperature_noai = self.optimal_temperature[0]
+        elif (self.temperature_noai > self.optimal_temperature[1]):
+            energy_noai = self.temperature_noai - self.optimal_temperature[1]
+            self.temperature_noai = self.optimal_temperature[1]
+
+        # Computing the Reward.
+        self.reward = energy_noai - energy_ai
+
+        # Scaling the reward
+        self.reward = 1e-3 * self.reward
+
+        # GETTING THE NEXT STATE
+        # Updating the atmospheric temperature
+        self.atmospheric_temperature =self.monthly_atmospheric_temperature[month]
+
+        # Updating the number of users
+        self.current_number_users += np.random.randint(-self.max_update_users, self.max_update_users)
+        if (self.current_number_users > self.max_number_users):
+            self.current_number_users = self.max_number_users
+        elif (self.current_number_users < self.min_number_users):
+            self.current_number_users = self.min_number_users
+
+        # Updating the rate of data
+        self.current_rate_date += np.random.randint(-self.max_update_data, self.max_update_data)
+        if (self.current_rate_data > self.max_rate_data):
+            self.current_rate_users = self.max_rate_data
+        elif (self.current_rate_data < self.min_rate_data):
+            self.current_rate_data = self.min_rate_data
+
+        # Computing the Delta of Intrinsic Temperature
+
+
+
     # MAKING A METHOD THAT RESETS THE ENVIRONMENT
 
     # MAKING A METHOD THAT GIVES US AT ANY TIME THE CURRENT STATE, THE LAST REWARD AND WHETHER THE GAME
