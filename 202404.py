878d2238635dcef1f813041781e14e3b326f1537
taich <taichi.kogure@gmail.com>
2025-03-18 16:29:25 +0800

    Update project SDK to Python 3.13 and clean up 202404.py
    Updated project configuration files to use Python 3.13 for consistency across environments. Modified 202404.py by adding a matplotlib backend selection and commenting out unused code for better readability and maintainability.

--- 202404.py
+++ 202404.py
@@ -1,6 +1,9 @@
-import numpy as np
+import matplotlib
+
+matplotlib.use('Qt5Agg')  # または 'Agg', 'WebAgg', 'TkAgg', など
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+
+import numpy as np
 
 
 # Define the func(x,y)
@@ -21,31 +24,31 @@ ax.plot_surface(x, y, z, color='y')
 plt.show()
 
 #%%
-
-x = 5
-y = -6
-
-x_plus_y = x + y
-x_minus_y = x - y
-x_times_y = x * y
-x_divided_by_y = x / y
-
-fifth_power_of_x = x ** 5
-square_root_of_x = x ** 0.5
-
-print(fifth_power_of_x)
-print(square_root_of_x)
-#%%
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
+#
+# x = 5
+# y = -6
+#
+# x_plus_y = x + y
+# x_minus_y = x - y
+# x_times_y = x * y
+# x_divided_by_y = x / y
+#
+# fifth_power_of_x = x ** 5
+# square_root_of_x = x ** 0.5
+#
+# print(fifth_power_of_x)
+# print(square_root_of_x)
+# #%%
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+#
+#
+# if __name__ == "__main__":
+#     app.run(debug=True)
+#
